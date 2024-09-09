0611c3ef8ca571825d3eedb41ec13235dcdd47b3
EmadAA <emadadil999@gmail.com>
2024-09-10 01:17:59 +0600

    Added the all functionality of a raffle draw  application

--- script.js
+++ script.js
@@ -10,11 +10,79 @@ const participantsName = [];
 
 inp.addEventListener("keypress", function(event){
     if(event.key === "Enter"){
-        let newNames= event.target.value.split(",");
-        if(newNames[0]===""){
-            newNames.foreach(name=> {
-                participantsName.push(name)
+        let newNames = event.target.value.split(", ");
+        console.log(newNames);
+        if(newNames[0]!==""){
+            newNames.forEach((name,index)=> {
+                participantsName.push(name);
+                let item= createListItem(name,index);
+                nameList.appendChild(item);
+               
             });
         }
+        event.target.value="";
     }
 });
+
+function createListItem(name, index){
+    let li = document.createElement('li');
+    li.className="list-group-item";
+    li.innerHTML=`${index+1}. `+ name; 
+    return li;
+}
+
+
+giveATry.addEventListener("click", function(){
+if(participantsName.length==0)
+{alert("Please add participants name first");
+
+}
+else{
+    let shuffleNames = shuffleArray(participantsName);
+    for(let i=0; i<shuffleNames.length; i++){
+        (function(i, count){
+            setTimeout(()=>{
+                let rand = Math.floor(Math.random()*shuffleNames.length);
+                display.value=shuffleNames[rand];
+
+                if(count==shuffleNames.length-1){
+                    if(!firstWinner.innerHTML){
+                        firstWinner.innerHTML=shuffleNames[rand];
+                        let  ind= shuffleNames.indexOf(shuffleNames[rand]);
+                        shuffleNames.splice(ind,1);
+                    }
+                    else if(!secondWinner.innerHTML){
+                        secondWinner.innerHTML=shuffleNames[rand];
+                        let  ind= shuffleNames.indexOf(shuffleNames[rand]);
+                        shuffleNames.splice(ind,1);
+                    }
+                    else if(!thirdWinner.innerHTML){
+                        thirdWinner.innerHTML=shuffleNames[rand];
+                        let  ind= shuffleNames.indexOf(shuffleNames[rand]);
+                        shuffleNames.splice(ind,1);
+                    }
+                    else{
+                        alert("All winners are selected");
+                    }
+                }
+                
+                }, i)
+        })(i*100, i)
+     
+    }
+
+}
+
+
+
+});
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  
